4e3df4bd94aefe2be055a8cf2440f4f88f3164eb
Arkadiy <arkada38@outlook.com>
2016-12-10 14:58:19 +0700

    DataContext and Properties are added to MainView

--- Evolyutsionika/Evolyutsionika/ViewModels/MainViewModel.cs
+++ Evolyutsionika/Evolyutsionika/ViewModels/MainViewModel.cs
@@ -4,6 +4,20 @@ namespace Evolyutsionika.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
+        #region Fields and Properties
+        string _SomeString;
+        public string SomeString
+        {
+            get { return _SomeString; }
+            set
+            {
+                if (value == _SomeString) return;
+                _SomeString = value;
+                RaisePropertyChanged(nameof(SomeString));
+            }
+        }
+        #endregion
+
         public MainViewModel()
         {
             
